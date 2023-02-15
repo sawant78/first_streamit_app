5f171a662fc1bd9afeffd39ba56595c00add9ab1
sawant78 <73849679+sawant78@users.noreply.github.com>
2023-02-15 15:50:38 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,10 @@
 
 import streamlit
 streamlit.title('My parents, new healthy dinner')
+
+streamlit.header('Breakfats menu')
+streamlit.text('Omega 3 and Blueberry oatmeal')
+streamlit.text('Kale , spinach')
+streamlit.text('Boiled Egg')
+
+
